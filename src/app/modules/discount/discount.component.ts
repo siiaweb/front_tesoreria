d0d09c354a6f9277d19b7c440fe0c98840a2a5e8
CarlosUJED <56055013+CarlosUJED@users.noreply.github.com>
2026-09-22 10:31:04 -0600

    test

--- src/app/modules/discount/discount.component.ts
+++ src/app/modules/discount/discount.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { DescuentosService } from '../../services/dashboard/descuentos/descuentos.service'; 
+import { DescuentosService } from '../../services/dashboard/descuentos/descuentos.service';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { PagoServiciosService } from '../../services/dashboard/pagoServicios/pagoservicios.service';
 import { Subscription } from 'rxjs';
 import { Descuentos } from '../../services/dashboard/descuentos/descuentos';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { EvoService } from '../../services/dashboard/evo.service';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { PagoOnline, DetPagoOnlineDTO, Carrito, PagoOnlineDTO } from '../shoppingcart/shoppingcart.component';
 
-declare const Checkout:any
+declare const Checkout: any
 
 @Component({
   selector: 'app-discount',
@@ -21,32 +22,32 @@ export class DiscountComponent implements OnInit {
   @BlockUI() blockUI!: NgBlockUI;
 
   det: any[] = [];
-  
+
   public Checkout() {
-    let body = <HTMLDivElement> document.body;
+    let body = <HTMLDivElement>document.body;
     let script = document.createElement('script');
     script.type = 'text/javascript';
-    script.innerHTML = "Checkout.configure({"+
-      "merchant: '1143891',"+
+    script.innerHTML = "Checkout.configure({" +
+      "merchant: '1143891'," +
       //"merchant: '1143891',"+
-        "order: {description: 'Pago de servicios',amount: '"+this.forma.get('pago_montoapagar')!.value+"',currency: 'MXN',id: '"+this.ID+"'},"+
-        "interaction: {merchant: {name: 'UJED',address: {line1: 'Calle Constitución 404, Zona Centro, 34100 Durango, Dgo.'}},"+
-            "displayControl : {billingAddress : 'HIDE'},},"+
-		    "session: {id:  '"+this.session_id+"'},"+
+      "order: {description: 'Pago de servicios',amount: '" + this.forma.get('pago_montoapagar')!.value + "',currency: 'MXN',id: '" + this.ID + "'}," +
+      "interaction: {merchant: {name: 'UJED',address: {line1: 'Calle Constitución 404, Zona Centro, 34100 Durango, Dgo.'}}," +
+      "displayControl : {billingAddress : 'HIDE'},}," +
+      "session: {id:  '" + this.session_id + "'}," +
       "});";
     script.async = false;
     script.defer = true;
     body.appendChild(script);
   }
-  
+
   private subscription!: Subscription;
 
   descuentos!: Descuentos[];
   descuentosdet!: Descuentos[];
 
-  session_id!:string;
-  successIndicator!:string;
-  ID!:string;
+  session_id!: string;
+  successIndicator!: string;
+  ID!: string;
 
   public items: any = "";
 
@@ -54,16 +55,16 @@ export class DiscountComponent implements OnInit {
 
   total = 0;
 
-  constructor( private _ds: DescuentosService, private fb: FormBuilder, private _ps: PagoServiciosService,
-               public router: Router, private _evo: EvoService ) { }
+  constructor(private _ds: DescuentosService, private fb: FormBuilder, private _ps: PagoServiciosService,
+    public router: Router, private _evo: EvoService) { }
 
   ngOnInit() {
 
     this.subscription = this._ps.getTsqpagosonline()
-    .subscribe((data: any) => {
-      this.ID = data;
-      //console.log(data);
-      })	
+      .subscribe((data: any) => {
+        this.ID = data;
+        //console.log(data);
+      })
 
     window.scroll(0, 0);
     this.crearFormulario();
@@ -77,7 +78,7 @@ export class DiscountComponent implements OnInit {
 
   }
 
-  valuesSelect(values: any){
+  valuesSelect(values: any) {
     //console.log(values);
     this.forma.get('pago_foldescto')!.setValue(values);
     this._ds.getDescuentoDet(values).subscribe(
@@ -86,48 +87,52 @@ export class DiscountComponent implements OnInit {
         console.log(descuentosdet);
         this.totalPrice(this.descuentosdet);
 
-      for (var i=0;i<this.descuentosdet.length;i++){
-      //console.log(items[i]);
-      this.det.push(this.descuentosdet[i].vdes_id+'_'+
-                    //this.descuentosdet[i].vdes_concepto+'_'+
-                    this.descuentosdet[i].vdes_cantidad+'_'+
-                    this.descuentosdet[i].vdes_punit+'_'+
-                    this.descuentosdet[i].vdes_regid+'_'+
-                    this.descuentosdet[i].vdes_porc+'_'+
-                    this.descuentosdet[i].vdes_a_pagar);
-      }
-      //console.log(this.det);
-      
+        for (var i = 0; i < this.descuentosdet.length; i++) {
+           const descuento:Descuentos=this.descuentosdet[i];
+           const rowDetalle:Carrito = {
+            dpago_idingreso: descuento.vdes_id,
+            dpago_cantidad: descuento.vdes_cantidad,
+            dpago_punit: descuento.vdes_punit,
+            regidescto:descuento.vdes_regid,
+            descto:descuento.vdes_porc,
+            dto_pagar:descuento.vdes_a_pagar,
+            Mount:descuento.vdes_a_pagar,
+            Descrip:descuento.vdes_concepto
+          }
+        this.det.push(rowDetalle)
+        }
+        //console.log(this.det);
+
       }
     )
   }
 
   totalPrice(datosdet: any) {
     this.total = 0;
-    for(let data of datosdet){
+    for (let data of datosdet) {
       this.total += parseFloat(data.vdes_a_pagar);
     }
     this.forma.get('pago_montoapagar')!.setValue(this.total);
     return this.total;
   }
 
-  get conceptoNovalido(){
+  get conceptoNovalido() {
     return this.forma.get('pago_concepto')!.invalid && this.forma.get('pago_concepto')!.touched
   }
 
-  get metodoPagoNovalido(){
+  get metodoPagoNovalido() {
     return this.forma.get('metodoPago')!.invalid && this.forma.get('metodoPago')!.touched
   }
 
-  getdescuentosNovalido(){
+  getdescuentosNovalido() {
     return this.forma.get('descuentos')!.invalid && this.forma.get('descuentos')!.touched
   }
 
-  crearFormulario(){
+  crearFormulario() {
 
     this.forma = this.fb.group({
       descuentos: ['', Validators.required],
-      pago_concepto: ['', [Validators.required,Validators.maxLength(150)]],
+      pago_concepto: ['', [Validators.required, Validators.maxLength(150)]],
       pago_foldescto: [''],
       pago_referencia: [this.ID],
       pago_montoapagar: [''],
@@ -138,9 +143,9 @@ export class DiscountComponent implements OnInit {
   }
 
 
-    // Limpieza segura en Angular (poner justo antes de configurar Checkout)
+  // Limpieza segura en Angular (poner justo antes de configurar Checkout)
   clearHostedCheckoutSessionStorage() {
-    const keys = ['HostedCheckout_sessionId','HostedCheckout_embedContainer','HostedCheckout_merchantState'];
+    const keys = ['HostedCheckout_sessionId', 'HostedCheckout_embedContainer', 'HostedCheckout_merchantState'];
     keys.forEach(k => {
       if (sessionStorage.getItem(k) !== null) {
         console.log('Borrando sessionStorage key:', k);
@@ -171,85 +176,36 @@ export class DiscountComponent implements OnInit {
   }
 
 
-  Pagar(){
+  Pagar() {
     this.blockUI.start();
-    if (this.forma.invalid){
-      return Object.values( this.forma.controls ).forEach( control =>{
+    if (this.forma.invalid) {
+      return Object.values(this.forma.controls).forEach(control => {
         control.markAsTouched();
         this.blockUI.stop();
       })
-    }else{
+    } else {
       console.log(this.forma);
-      this._ps.create(this.forma.value).subscribe(master => {
 
-        this._ps.createDetalle(master.pago_folpago.toString(),this.det).subscribe(detalle =>{
-          this.blockUI.stop();
-          Swal.fire({icon: 'success',title: 'Datos Guardados',text: 'Se te redireccionara al portal de pago',showConfirmButton: false,timer: 3000});
-          sessionStorage.removeItem('shoppingCart');
-          this._evo.getEvo(this.total,null).subscribe(
-            (variables:any) => {
-              this.session_id = variables.session_id;
-              this.successIndicator = variables.successIndicator;
-              //this.router.navigate(['dashboard']);
-                    
-              
-              this.clearHostedCheckoutSessionStorage();
+      const pagoOnline:PagoOnline = (this.forma.getRawValue() as PagoOnline);
+      
+      const detalle: Array<DetPagoOnlineDTO> = this.det.map((e:Carrito)=>{
+        return {
+            idingreso:e.dpago_idingreso,
+            cantidad:e.dpago_cantidad,
+            punit:e.dpago_punit
+        }
+      });
+      const entity:PagoOnlineDTO = { 
+        usuaid:pagoOnline.pago_usuaid, 
+        concepto:pagoOnline.pago_concepto,
+        montoapagar:pagoOnline.pago_montoapagar,
+        user:pagoOnline.pago_usuaid,
+        detalle: detalle 
+      }
 
-              const ck = (window as any).Checkout;
-              if (!ck) {
-                console.error('Checkout no disponible en window');
-              } else {
-                ck.configure({ session: { id: this.session_id } });
-                console.log('Checkout configurado con:', this.session_id);
 
-                // 1) mostrar overlay primero
-                this.showEvoOverlay();
-
-                // 2) esperar un tick para que Angular pinte el overlay y el contenedor
-                setTimeout(() => {
-                  try {
-                    // pasar height: '100%' no siempre funciona; el contenedor controla la altura
-                    const res = ck.showEmbeddedPage('#evo-embed-container');
-                    if (res && typeof res.then === 'function') {
-                      res.then(() => console.log('showEmbeddedPage completado')).catch((e:any) => {
-                        console.error('Error showEmbeddedPage:', e);
-
-                        this.closeEvoModal();
-                      });
-                    }
-                    
-                  } catch (e) {
-                    console.error('Error invocando showEmbeddedPage:', e);
-                    this.closeEvoModal();
-                  }
-                }, 200); // 200ms es suficiente si el overlay ya está visible
-              }
-
-              sessionStorage.MasterID = master.pago_referencia.toString()
-
-            }
-          )
-
-        },
-        error => {
-          console.log(error);
-          this.blockUI.stop();
-          Swal.fire({
-            title: 'ERROR!!!',
-            text: error.error.message,
-            icon: 'error'});
-        });
-
-      },
-      error => {
-        console.log(error);
-        this.blockUI.stop();
-        Swal.fire({
-          title: 'ERROR!!!',
-          text: error.error.message,
-          icon: 'error'});
-      })
+     
     }
-    
+
   }
 }
